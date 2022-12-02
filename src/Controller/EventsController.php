2303ff1e39737ceccbf46829f7733d143d68ba9f
Felix Budin <felix_budin@gmx.net>
2022-12-02 11:54:42 +0100

    created crud

--- src/Controller/EventsController.php
+++ src/Controller/EventsController.php
@@ -1,18 +1,102 @@
 <?php
-
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\EventsRepository;
+
+use App\Entity\Events;
+use App\Form\EventsType;
+
 
 class EventsController extends AbstractController
 {
-    #[Route('/events', name: 'app_events')]
-    public function index(): Response
-    {
-        return $this->render('events/index.html.twig', [
-            'controller_name' => 'EventsController',
-        ]);
+  #[Route('/', name: 'events')]
+  public function index(ManagerRegistry $doctrine): Response
+  {
+    $events = $doctrine->getRepository(Events::class)->findAll();
+
+    return $this->render('events/index.html.twig', ['events' => $events]);
+  }
+  
+  #[Route('/create', name: 'events_create')]
+  public function create(Request $request, ManagerRegistry $doctrine): Response
+  {
+      $events = new Events();
+      $form = $this->createForm(EventsType::class, $events);
+
+      $form->handleRequest($request);
+
+
+      if ($form->isSubmitted() && $form->isValid()) {
+          
+
+
+          $events = $form->getData();
+          
+          $em = $doctrine->getManager();
+          $em->persist($events);
+          $em->flush();
+
+          $this->addFlash(
+              'notice',
+              'Event Added'
+              );
+    
+          return $this->redirectToRoute('events');
+      }
+
+    return $this->render('events/create.html.twig', ['form' => $form->createView()]);
+  }
+
+  
+#[Route('/edit/{id}', name: 'events_edit')]
+public function edit(Request $request, ManagerRegistry $doctrine, $id): Response
+{
+    $events = $doctrine->getRepository(Events::class)->find($id);
+    $form = $this->createForm(EventsType::class, $events);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $events = $form->getData();
+        
+        $em = $doctrine->getManager();
+        $em->persist($events);
+        $em->flush();
+        $this->addFlash(
+             'notice',
+             'Event Edited'
+             );
+
+        return $this->redirectToRoute('events');
     }
+
+    return $this->render('events/edit.html.twig', ['form' => $form->createView()]);
+}
+
+  #[Route('/details/{id}', name: 'events_details')]
+  public function details(ManagerRegistry $doctrine, $id): Response
+  {
+      $events = $doctrine->getRepository(Events::class)->find($id);
+
+      return $this->render('events/details.html.twig', ['events' => $events]);
+  }
+
+  #[Route('/delete/{id}', name: 'delete_events')]
+  public function delete(ManagerRegistry $doctrine, $id){
+    $em = $doctrine->getManager();
+    $events = $em->getRepository(Events::class)->find($id);
+    $em->remove($events);
+    
+    $em->flush();
+    $this->addFlash(
+        'notice',
+        'Event Removed'
+    );
+    
+    return $this->redirectToRoute('events');
+}
 }
---
+++ src/Form/EventsType.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Events;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class EventsType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+      $builder
+          ->add('name', TextType::class, [
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+          ->add('name', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+          ->add('image', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+        ])
+          ->add('description', TextType::class, [
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+          ->add('capacity', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+        ])
+        ->add('email', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+        ])
+        ->add('phone', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+        ])
+        ->add('address', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+        ])
+        ->add('url', TextType::class, [
+            'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+        ])
+          ->add('type', ChoiceType::class, [
+              'choices' => ['Music' => 'Music', 'Sport' => 'Sport', 'Movie' => 'Movie', 'Theatre' => 'Theatre'],
+              'attr' => ['class' => 'form-control', 'style' => 'margin-bottom:15px']
+          ])
+         ->add('date', DateTimeType::class, [
+              'attr' => ['style' => 'margin-bottom:15px']
+          ])
+          ->add('save', SubmitType::class, [
+              'label' => 'Create events',
+              'attr' => ['class' => 'btn-primary', 'style' => 'margin-bottom:15px']
+          ]);
+  }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+      $resolver->setDefaults([
+          'data_class' => Events::class,
+      ]);
+  }
+}
